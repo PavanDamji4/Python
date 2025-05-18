e14cbc9c51a0658fedee14b396a3a3c462108d99
Pavan Damji <izon2709@gmail.com>
2025-05-18 17:56:29 +0530

    Update PC_01_Basic.py

--- PC_01_Basic.py
+++ PC_01_Basic.py
@@ -1,14 +1,14 @@
 # This is the first program of this Python course.
 # It demonstrates the use of the 'print' function.
 
-print("Hello world, I am Pavan Vishnu Damji")  # Prints a string message
+print("Hello world, I am Pavan Vishnu Damji")  
 
-print(5)           # Prints the number 5
+print(5)           
 
-print(5 + 4)       # Adds 5 and 4, then prints the result (9)
+print(5 + 4)       
 
 # print(hello)     # This would cause an error because hello is not defined (it's not in quotes)
 
-print(5 * 2)       # Multiplies 5 by 2, then prints the result (10)
+print(5 * 2)       
 
-print(6 / 2)       # Divides 6 by 2, then prints the result (3.0)
+print(6 / 2)       
