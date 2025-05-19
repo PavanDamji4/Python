9ea9e210618591bf3f1b2eeb9cb3b64c0bb16ab8
Pavan Damji <izon2709@gmail.com>
2025-05-19 18:17:23 +0530

    Add files via upload

---
+++ PC_02_Comments,printfunc.py
@@ -0,0 +1,25 @@
+# This is the 2nd program of this Python Course
+# In this, we are working on comments, escape sequences, and print statements
+
+print("Hello,", " I am Pavan")
+# Prints two strings with a space between them by default
+
+print("hello World we are learning \"Python\", and this is 2nd chapter")
+# Prints the string and uses \" to include double quotes inside the string
+
+print('Hello world, welcome to "Python"')
+
+print("I am a Good boy \nI study daily")
+# \n is an escape sequence that adds a new line
+
+print("micro", "organisms", sep="-")
+# sep is used to change the separator between items from space to "-"
+
+# This is a single-line comment
+
+"""This is a multi-line comment
+line 1
+line 2
+line 3
+"""
+# Triple quotes are often used for multi-line comments or docstrings
