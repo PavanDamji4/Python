e585503d899db79e3ffb08ee2f183456278b4e64
Pavan Damji <izon2709@gmail.com>
2025-05-20 17:01:34 +0530

    Add files via upload

---
+++ PC_03_Variables & Datatypes.py
@@ -0,0 +1,13 @@
+# 3rd code of this Python course
+# It is about variables and data types
+
+a = 23         # Integer value assigned to variable 'a'
+b = "pavan"    # String value assigned to variable 'b'
+c = 2.3        # Float (decimal) value assigned to variable 'c'
+d = True       # Boolean value (True/False) assigned to variable 'd'
+
+# type() function is used to get the datatype of a variable
+print("datatype of a is :", type(a))
+print("datatype of b is :", type(b))
+print("datatype of c is :", type(c))
+print("datatype of d is :", type(d))
