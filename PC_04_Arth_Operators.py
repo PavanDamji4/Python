caa79eac0dbfb59cc63e3ddcd68fb92967c0048f
Pavan Damji <izon2709@gmail.com>
2025-05-21 18:22:49 +0530

    Add files via upload

---
+++ PC_04_Arth_Operators.py
@@ -0,0 +1,12 @@
+# 4th code of this Python course
+# It is about Arithmetic Operations
+
+a = 6
+b = 2
+
+print("Addition : ", a + b)             # Adds a and b → 6 + 2 = 8
+print("Substraction : ", a - b)        # Subtracts b from a → 6 - 2 = 4
+print("Multiplication : ", a * b)      # Multiplies a and b → 6 * 2 = 12
+print("Diviison : ", a / b)            # Divides a by b (returns float) → 6 / 2 = 3.0
+print("Square of a by b : ", a ** b)   # a raised to the power of b → 6^2 = 36
+print("Floor : ", a // b)              # Floor division (ignores decimal part) → 6 // 2 = 3
