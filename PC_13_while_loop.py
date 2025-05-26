51a8030d272bf6e15fb41833324df4cbadde382a
Pavan Damji <izon2709@gmail.com>
2025-05-26 17:21:12 +0530

    Add files via upload

---
+++ PC_13_while_loop.py
@@ -0,0 +1,13 @@
+# This is 13th code of this Python Course
+# It demonstrates the use of while loop
+
+a = 1
+while a <= 20:
+    print(a)
+    a += 1
+
+b = 1
+while b <= 20:
+    if b % 2 == 0:
+        print(b)
+    b += 1
---
+++ PC_14_do_while_loop.py
@@ -0,0 +1,8 @@
+# This is 14th code of this Python Course
+# It demonstrates the use of do-while loop
+
+while True:  # loop will run at least once like do-while
+    a = int(input("Enter a number "))
+    print("You Entered", a)
+    if a > 50:  # loop stops when condition is met
+        break
