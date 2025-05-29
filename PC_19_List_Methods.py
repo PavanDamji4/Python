ed332e463793d54133c0eb1e7d830534dde6d23f
Pavan Damji <izon2709@gmail.com>
2025-05-29 17:03:46 +0530

    Add files via upload

---
+++ PC_19_List_Methods.py
@@ -0,0 +1,20 @@
+# This is 19th code of this Python Course
+# Demonstrates list methods
+
+l = [101, 102, 103, 104, 104, 105]
+print(l)
+
+# uncomment and run one method to see the specific behaviour
+
+# l.append("shubham")
+# print(l.count(104))
+# l.sort()
+# l.sort(reverse=True)
+# l.reverse()
+# print(l.index(103))
+
+# m = l.copy()
+# m.append(233)
+# print(m)
+
+print(l)
---
+++ PC_20_tuple.py
@@ -0,0 +1,14 @@
+# This is 20th code of this Python Course
+# This code demonstrates the use of tuple
+
+tuple = (101, 102, 103, 104, 105, 106, 107, 108, 109, 110)
+print(tuple)              # prints entire tuple
+print(tuple[3])           # element at index 3
+print(tuple[0:3])         # elements from index 0 to 2
+print(tuple[0:6:2])       # elements from 0 to 5 with step 2
+print(tuple[:-1])         # all elements except last
+print(tuple[:])           # entire tuple
+print(tuple[-5:])         # last 5 elements
+
+print(102 in tuple)
+print("yash" in tuple)
