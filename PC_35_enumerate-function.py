b4bb250e0ceb16bc8cfe584de4912128f7ec47d0
Pavan Damji <izon2709@gmail.com>
2025-06-08 12:37:04 +0530

    Add files via upload

---
+++ PC_35_enumerate-function.py
@@ -0,0 +1,7 @@
+# This is 35th code of this python course
+# This code demonstrates the use of enumerate function
+
+list = [0,1,2,3,4,5,6,7,8,9,10]
+
+for i,value in enumerate(list):
+    print(f"The {value} is at index {i} ")
---
+++ PC_36_import-word.py
@@ -0,0 +1,16 @@
+# This code is 36th code of this python course
+# This code demonstrates the use of import keyword for importing built in files and user defined files
+
+
+from PC_26_Set import s as file
+
+# import math as m
+
+# print(dir(m))
+# print(m.sqrt(25))
+# print(m.pow(3,2))
+# print(m.pi)
+
+print(file.s)
+
+# if __name__ == "__main__" is basically used for using the function from another file
