2f2094cd92e74f18eedcb2fc0cd7265048b041ec
Pavan Damji <izon2709@gmail.com>
2025-06-27 18:35:31 +0530

    Add files via upload

---
+++ PC_66_Custom-module.py
@@ -0,0 +1,7 @@
+import PC_50_Inheritance as cm
+from PC_50_Inheritance import child
+
+cm.c=child()
+cm.c.show1()
+cm.c.show2()
+
---
+++ PC_67_shutil.py
@@ -0,0 +1,11 @@
+# This is 67th code of this python course
+# this code demonstrates the use of shutil module in python
+
+import shutil
+
+shutil.copy("PC_01_Basic.py","PC_01_Basics_part2.py")
+
+shutil.copytree("PC_37_Data","PC_37_Data-2")
+
+shutil.move("Folder 2","PC_37_Data/Folder 2")
+
